0df9e7adc64f19d08808050591fc83ef772b97ad
Nicolas Sikora <nsikora@student.42.fr>
2019-10-18 14:33:23 +0200

    libft updated

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsikora <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/10 13:48:12 by nsikora           #+#    #+#             */
+/*   Updated: 2019/10/18 13:56:49 by nsikora          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*c_dst;
+	unsigned char	*c_src;
+
+	i = 0;
+	c_dst = (unsigned char *)dst;
+	c_src = (unsigned char *)src;
+	while (i < n && c_src[i])
+	{
+		c_dst[i] = c_src[i];
+		i++;
+	}
+	return (dst);
+}
